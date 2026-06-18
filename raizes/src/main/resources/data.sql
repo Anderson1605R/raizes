3b6cb4aecf139cde3dc9f922610f37b0c1191639
Anderson <andrilsilva1994@gmail.com>
2026-06-18 17:11:09 -0300

    atualizaçoes do seed

--- raizes/src/main/resources/data.sql
+++ raizes/src/main/resources/data.sql
@@ -6,12 +6,12 @@ INSERT INTO unidade (nome, localizacao) VALUES ('Raízes Paulista', 'São Paulo-
 INSERT INTO produto (nome, descricao, preco_base) VALUES ('Tapioca de Carne de Sol', 'Tapioca recheada com carne de sol e queijo coalho', 15.50);
 INSERT INTO produto (nome, descricao, preco_base) VALUES ('Cuscuz Completo', 'Cuscuz com ovo, charque e queijo', 12.00);
 
--- 3. Inserindo o Estoque (Relacionando Unidade e Produto)
--- Ex: A unidade 1 (Recife) tem 50 Tapiocas e 30 Cuscuz disponíveis
+-- 3. Inserindo o Estoque (Relacionando a Unidade 1 aos Produtos 1 e 2)
 INSERT INTO estoque (unidade_id, produto_id, quantidade) VALUES (1, 1, 50);
 INSERT INTO estoque (unidade_id, produto_id, quantidade) VALUES (1, 2, 30);
 
--- 4. Inserindo o Usuário para Teste de Login
--- A senha abaixo é o Hash BCrypt correspondente a "Senha@123" para cumprir a regra da LGPD
+-- 4. Inserindo o Usuário para Teste de Login (Com proteção contra quebra da aplicação)
+-- O ON CONFLICT impede que o Spring trave caso você reinicie a API e o e-mail já exista.
 INSERT INTO tb_usuario (nome, email, senha_hash, perfil, pontos_fidelidade) 
-VALUES ('Cliente Teste', 'cliente@exemplo.com', '$2a$10$EblZqNptyYvcLm/VwDCVAuAw5QOTP.1/2B.pBdbGfE//N7Vw3qDZm', 'CLIENTE', 0);
+VALUES ('Cliente Teste', 'cliente@exemplo.com', '$2a$10$EblZqNptyYvcLm/VwDCVAuAw5QOTP.1/2B.pBdbGfE//N7Vw3qDZm', 'CLIENTE', 0)
+ON CONFLICT (email) DO NOTHING;
